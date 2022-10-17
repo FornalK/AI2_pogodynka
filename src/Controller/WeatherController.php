1a43f41b796868a8a9abbaede40bb6fb37bdea0d
Kacper Fornalczyk <fk46509@zut.edu.pl>
2022-10-17 10:11:33 +0200

    routing, view, controller

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\City;
+use App\Repository\MeasurmentRepository;
+use App\Repository\CityRepository;
+
+class WeatherController extends AbstractController
+{
+    // public function cityAction(City $city, MeasurmentRepository $measurementRepository): Response
+    // {
+    //     $measurements = $measurementRepository->findByCity($city);
+
+    //     return $this->render('weather/city.html.twig', [
+    //         'city' => $city,
+    //         'measurements' => $measurements
+    //     ]);
+    // }
+
+    public function cityAction(string $country_name,
+                               string $city_name,
+                               MeasurmentRepository $measurementRepository,
+                               CityRepository $cityRepository): Response
+    {
+        $city = $cityRepository->findCityByCountryAndCityName($country_name, $city_name);
+        $measurements = $measurementRepository->findByCity($city);
+
+        return $this->render('weather/city.html.twig', [
+            'city' => $city,
+            'measurements' => $measurements
+        ]);
+    }
+}
---
+++ src/Entity/Measurment.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeasurmentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeasurmentRepository::class)]
+class Measurment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?City $city = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 1, nullable: true)]
+    private ?string $temperature = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 1, nullable: true)]
+    private ?string $wind = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $cloudiness_level = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCityId(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCityId(?City $city_id): self
+    {
+        $this->city = $city_id;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(?string $temperature): self
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getWind(): ?string
+    {
+        return $this->wind;
+    }
+
+    public function setWind(string $wind): self
+    {
+        $this->wind = $wind;
+
+        return $this;
+    }
+
+    public function getCloudinessLevel(): ?int
+    {
+        return $this->cloudiness_level;
+    }
+
+    public function setCloudinessLevel(?int $cloudiness_level): self
+    {
+        $this->cloudiness_level = $cloudiness_level;
+
+        return $this;
+    }
+}
